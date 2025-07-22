35db3dbce3d1e9412d74a052dc2d7647b52d0894
welnecker <janiofdr@gmail.com>
2025-07-22 12:55:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -123,6 +123,13 @@ def carregar_objetivos_por_status():
     gatilhos_disponiveis = carregar_objetivos_por_status()
     historico = st.session_state.get("mensagens", [])
 
+    def construir_prompt_mary():
+     perfil = carregar_perfil_mary()
+    gatilho_ativo = st.session_state.get("gatilho_mary", "Nenhum")
+    gatilhos_disponiveis = carregar_objetivos_por_status()
+    historico = st.session_state.get("mensagens", [])
+
+    
     inicio_padrao = ""
     if not historico:
         inicio_padrao = (
